34de8c212a54310ad52841c6d2c80990265d0c4b
Nikita Kuznetsov <Nikita@Nikitas-Mac-mini.local>
2014-08-25 19:00:17 -0700

    Undead styling and begin on plate calculator.

--- src/js/cleanlifts/workout.js
+++ src/js/cleanlifts/workout.js
@@ -192,13 +192,18 @@ cleanlifts.controller('ChangeWeightController',
     function($scope,   $state,   $stateParams) {
       $scope.lift = $stateParams['lift'];
       $scope.weight = Number($scope.user.working_weight[$scope.lift]);
+      $scope.plates = {};
+
+      calculatePlates();
 
       $scope.decreaseWeight = function() {
         $scope.weight = $scope.weight - 5;
+        calculatePlates();
       };
 
       $scope.increaseWeight = function() {
         $scope.weight = $scope.weight + 5;
+        calculatePlates();
       };
 
       $scope.saveWeight = function() {
@@ -210,6 +215,35 @@ cleanlifts.controller('ChangeWeightController',
       $scope.cancelChanges = function() {
         window.history.back();
       };
+
+      function calculatePlates() {
+        var each_side = ($scope.weight - 45) / 2;
+        var plates = {'45': 0, '35': 0, '25': 0, '10': 0, '5': 0, '2.5': 0};
+        while (each_side > 0) {
+          if (each_side >= 45) {
+            plates['45']++;
+            each_side -= 45;
+          } else if (each_side >= 35) {
+            plates['35']++;
+            each_side -= 35;
+          } else if (each_side >= 25) {
+            plates['25']++;
+            each_side -= 25;
+          } else if (each_side >= 10) {
+            plates['10']++;
+            each_side -= 10;
+          } else if (each_side >= 5) {
+            plates['5']++;
+            each_side -= 5;
+          } else if (each_side >= 2.5) {
+            plates['2.5']++;
+            each_side -= 2.5;
+          } else {
+            break;
+          }
+        }
+        $scope.plates = plates;
+      }
     }
   ]
 );
