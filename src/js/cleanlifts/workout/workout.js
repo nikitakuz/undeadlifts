7b3b72de2870413969cc8cd87a2b4944f1e60876
Nikita Kuznetsov <Nikita@Nikitas-Mac-mini.local>
2014-09-10 17:24:51 -0700

    Lift success/failure messaging.

--- src/js/cleanlifts/workout/workout.js
+++ src/js/cleanlifts/workout/workout.js
@@ -140,49 +140,4 @@ cleanlifts.controller('WorkoutController',
     }
   ]
 );
-cleanlifts.controller('LiftController',
-  [         '$rootScope', '$scope', '$state',
-    function($rootScope,   $scope,   $state) {
-      $scope.classname = { 1: 'one-set', 2: 'two-sets', 3: 'three-sets', 4: 'four-sets', 5: 'five-sets' }[$scope.lift.sets.length];
 
-      $scope.updateSet = function(si) {
-        var s = $scope.lift.sets[si];
-        if (s.completed === undefined) {
-          s.completed = s.target;
-        } else if (s.completed === 0) {
-          delete s.completed; // Note: assigning undefined to s.completed causes problems when saving
-        } else {
-          s.completed--;
-        }
-        if (isLiftComplete()) {
-          console.log('TODO: Handle completed lift.');
-        }
-        if (isLiftSuccess()) {
-          console.log('TODO: Handle successful lift.');
-        }
-      };
-
-      function isLiftComplete() {
-        var complete = true;
-        var sets = $scope.lift.sets;
-        for (var i = 0; i < sets.length; i++) {
-          if (typeof sets[i].completed !== "number") {
-            complete = false;
-          }
-        }
-        return complete;
-      }
-
-      function isLiftSuccess() {
-        var success = true;
-        var sets = $scope.lift.sets;
-        for (var i = 0; i < sets.length; i++) {
-          if (sets[i].completed !== sets[i].target) {
-            success = false;
-          }
-        }
-        return success;
-      }
-    }
-  ]
-);
