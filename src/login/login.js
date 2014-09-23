5f27458018c96243393ffc79653567bc71cea492
Nikita Kuznetsov <Nikita@Nikitas-Mac-mini.local>
2014-09-23 13:59:06 -0700

    Login and signup error handling.

--- src/login/login.js
+++ src/login/login.js
@@ -1,8 +1,99 @@
 (function() {
+  var login = angular.module('login', ['firebase']);
+  var loginControllerScope;
+  login.controller('LoginController',
+    [         '$scope', '$timeout',
+      function($scope,   $timeout) {
+        loginControllerScope = $scope;
+        $scope.focusEmail = true;
+        $scope.focusPassword = false;
+        $scope.emailError = false;
+        $scope.passwordError = false;
+
+        $scope.loginButtonText = 'Login to my account';
+
+        $scope.login = function() {
+          $scope.loginButtonText = 'Logging in...';
+          fbsl.login('password', { email: $scope.email, password: $scope.password, rememberMe: true });
+//          $scope.password = '';
+        };
+
+        $scope.handleError = function(error) {
+          $scope.loginButtonText = 'Login to my account';
+          $scope.emailError = false;
+          $scope.passwordError = false;
+          var message = {
+            'INVALID_EMAIL': 'The email address entered is invalid.',
+            'INVALID_USER': 'No account exists for this email address.',
+            'INVALID_PASSWORD': 'The password entered is incorrect.'
+          }[error.code];
+          if (error.code === 'INVALID_EMAIL' || error.code === 'INVALID_USER') {
+            $scope.emailError = true;
+            focusEmail();
+          } else if (error.code === 'INVALID_PASSWORD') {
+            $scope.passwordError = true;
+            focusPassword();
+          }
+/*
+          $scope.$apply(function() {
+            $scope.error = message || 'Unknown error';
+          });
+*/
+        };
+
+        $scope.$watch('form.email.$viewValue', function(newVal, oldVal) {
+          if ($scope.emailError && newVal !== oldVal) {
+            $scope.emailError = false;
+          }
+        });
+
+        $scope.$watch('form.password.$viewValue', function(newVal, oldVal) {
+          if ($scope.passwordError && newVal !== oldVal) {
+            $scope.passwordError = false;
+          }
+        });
+
+        function focusEmail() {
+          $scope.$apply(function() {
+            $scope.focusEmail = true;
+          });
+        }
+
+        function focusPassword() {
+          $scope.$apply(function() {
+            $scope.focusPassword = true;
+          });
+        }
+      }
+    ]
+  );
+
+  login.directive('focus',
+    function() {
+      return {
+        restrict: 'A',
+        scope: { focus: '=focus' },
+        link: function(scope, element, attrs) {
+          scope.$watch('focus', function(newVal, oldVal) {
+            if (scope.focus) {
+              element[0].focus()
+              element[0].select();
+              scope.focus = false;
+            }
+          });
+        }
+      }
+    }
+  );
+
   var fb = new Firebase('https://cleanlifts.firebaseio.com');
   var fbsl = new FirebaseSimpleLogin(fb, function(error, user) {
     if (error) {
-      console.log(error);
+      if (loginControllerScope) {
+        loginControllerScope.handleError(error);
+      } else {
+        console.log(error);
+      }
     } else if (user) {
       console.log('User logged in. Redirecting to app...');
       if (location.replace) {
@@ -17,16 +108,4 @@
       });
     }
   });
-
-  var login = angular.module('login', ['firebase']);
-  login.controller('LoginController',
-    [         '$scope',
-      function($scope) {
-        $scope.login = function() {
-          fbsl.login('password', { email: $scope.email, password: $scope.password, rememberMe: true });
-          $scope.password = '';
-        };
-      }
-    ]
-  );
 })();
--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,4 +1,77 @@
 (function() {
+  var signup = angular.module('signup', ['firebase']);
+  var signupControllerScope;
+  signup.controller('SignUpController',
+    [         '$scope',
+      function($scope) {
+        signupControllerScope = $scope;
+        $scope.focusEmail = true;
+        $scope.focusPassword = false;
+        $scope.emailError = false;
+        $scope.passwordError = false;
+
+        $scope.signupButtonText = 'Create my free account';
+
+        $scope.signup = function() {
+          $scope.signupButtonText = 'Creating account...';
+          var email = $scope.email;
+          var password = $scope.password;
+          fbsl.createUser(email, password, function(error, user) {
+            if (error) {
+              $scope.signupButtonText = 'Create my free account';
+              handleAuthError(error);
+            } else {
+              $scope.$apply(function() {
+                $scope.signupButtonText = 'Logging in...';
+              });
+              fbsl.login('password', {
+                email: email,
+                password: password
+              });
+            }
+          });
+        };
+
+        $scope.$watch('form.email.$viewValue', function(newVal, oldVal) {
+          if ($scope.emailError && newVal !== oldVal) {
+            $scope.emailError = false;
+          }
+        });
+
+        function handleAuthError(error) {
+          var msg = {
+            'EMAIL_TAKEN': 'This email address is already taken.',
+            'INVALID_EMAIL': 'This email address is invalid.'
+          };
+          if (error.code === 'EMAIL_TAKEN' || error.code === 'INVALID_EMAIL') {
+            $scope.emailError = true;
+            $scope.focusEmail = true;
+          }
+          $scope.error = msg[error.code] || 'Unrecognized error.';
+          $scope.$digest();
+        }
+      }
+    ]
+  );
+
+  signup.directive('focus',
+    function() {
+      return {
+        restrict: 'A',
+        scope: { focus: '=focus' },
+        link: function(scope, element, attrs) {
+          scope.$watch('focus', function(newVal, oldVal) {
+            if (scope.focus) {
+              element[0].focus()
+              element[0].select();
+              scope.focus = false;
+            }
+          });
+        }
+      }
+    }
+  );
+
   var fb = new Firebase('https://cleanlifts.firebaseio.com');
   var fbsl = new FirebaseSimpleLogin(fb, function(error, user) {
     if (error) {
@@ -17,36 +90,4 @@
       });
     }
   });
-
-  var signup = angular.module('signup', ['firebase']);
-  signup.controller('SignUpController',
-    [         '$scope',
-      function($scope) {
-        $scope.signup = function() {
-          var email = $scope.email;
-          var password = $scope.password;
-          $scope.password = '';
-          fbsl.createUser(email, password, function(error, user) {
-            if (error) {
-              handleAuthError(error);
-            } else {
-              fbsl.login('password', {
-                email: email,
-                password: password
-              });
-            }
-          });
-        };
-
-        function handleAuthError(error) {
-          var msg = {
-            'EMAIL_TAKEN': 'This email address is already taken.',
-            'INVALID_EMAIL': 'This email address is invalid.'
-          };
-          $scope.error = msg[error.code] || 'Unrecognized error.';
-          $scope.$digest();
-        }
-      }
-    ]
-  );
 })();
