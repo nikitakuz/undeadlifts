e328e4179018711a94fcca9e9db3b8bb3a7a8e88
Nikita Kuznetsov <Nikita@Nikitas-Mac-mini.local>
2014-09-25 21:48:07 -0700

    Move lifts list into a service.

--- src/user/create-routine/select-lifts/select-lifts.js
+++ src/user/create-routine/select-lifts/select-lifts.js
@@ -13,12 +13,12 @@ undeadlifts.config(
 );
 
 undeadlifts.controller('SelectLiftsController',
-  [         '$window', '$scope',
-    function($window,   $scope) {
-      var BARBELL = 0;
-      var DUMBELL = 1;
-      var CABLE = 2;
-      var OTHER = 3;
+  [         '$window', '$scope', 'lifts',
+    function($window,   $scope,   lifts) {
+      var BARBELL = lifts.BARBELL;
+      var DUMBELL = lifts.DUMBELL;
+      var CABLE = lifts.CABLE;
+      var OTHER = lifts.OTHER;
 
       $scope.types = [
         { name: 'All Lifts', type: -1 },
@@ -27,36 +27,8 @@ undeadlifts.controller('SelectLiftsController',
         { name: 'Cable Lifts', type: CABLE },
         { name: 'Other', type: OTHER }
       ];
-      $scope.type = $scope.types[0];
 
-      $scope.lifts = [
-        { name: 'Squat',              type: BARBELL,  sets: 5, reps: 5 },
-        { name: 'Arnold Press',       type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Barbell Bench',      type: BARBELL,  sets: 5, reps: 5 },
-        { name: 'Barbell Curl',       type: BARBELL,  sets: 3, reps: 8 },
-        { name: 'Barbell Press',      type: BARBELL,  sets: 5, reps: 5 },
-        { name: 'Barbell Row',        type: BARBELL,  sets: 5, reps: 5 },
-        { name: 'Barbell Shrug',      type: BARBELL,  sets: 3, reps: 8 },
-        { name: 'Cable Crunches',     type: CABLE,    sets: 5, reps: 5 },
-        { name: 'Cable Row',          type: CABLE,    sets: 5, reps: 5 },
-        { name: 'Chin Up',            type: OTHER,    sets: 5, reps: 5 },
-        { name: 'Deadlift',           type: BARBELL,  sets: 1, reps: 5 },
-        { name: 'Dips',               type: OTHER,    sets: 5, reps: 5 },
-        { name: 'Dumbell Bench',      type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Dumbell Curl',       type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Dumbell Press',      type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Dumbell Row',        type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Dumbell Shrug',      type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Goblet Squat',       type: DUMBELL,  sets: 5, reps: 5 },
-        { name: 'Hyperextensions',    type: OTHER,    sets: 2, reps: 10 },
-        { name: 'Pistol Squats',      type: OTHER,    sets: 5, reps: 5 },
-        { name: 'Power Clean',        type: BARBELL,  sets: 5, reps: 5 },
-        { name: 'Push Up',            type: OTHER,    sets: 3, reps: 10 },
-        { name: 'Pull Over',          type: OTHER,    sets: 3, reps: 10 },
-        { name: 'Pull Up',            type: OTHER,    sets: 3, reps: 10 },
-        { name: 'Reverse Crunches',   type: OTHER,    sets: 3, reps: 10 },
-        { name: 'Tricep Extensions',  type: CABLE,    sets: 5, reps: 5 }
-      ];
+      $scope.type = $scope.types[0];
 
       $scope.selectLift = function(lift) {
         if ($scope.selected.indexOf(lift) > -1) {
